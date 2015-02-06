443c5f16770fb18a31a385402c0004ab24c0d93c
Evan-Diehm <evan.diehm@gmail.com>
2015-02-05 21:18:50 -0600

    added defend function

--- script.js
+++ script.js
@@ -3,37 +3,64 @@ var character = null;
 	characterClass = null;
 	charLvl = 1;
 	//set in enemycration();
+	charHealth = 10;
 	enemyHealth = 10;
 function characterCreation(){
 	character = document.getElementById('characterName').value;
 	var	 charClass = document.getElementById('characterClass');
 	characterClass = charClass.options[charClass.selectedIndex].value;
 
-	//document.cookie = "character=Artomous" ;
+	//document.cookie = "character=" + characterName ;
 	
 	hide('characterSelection');
-	show('chapterOne')
+	show('attack');
 }
 
 function attack(){
 	var youHit = Math.floor(Math.random() * 2);
 
 	if(youHit){
+		console.log("you hit");
 		var damage = Math.floor(Math.random() * 5 + 1);
+		console.log("damage=" + damage);
 		enemyHealth -= damage;
+		console.log("enemy Health=" + enemyHealth);
 		if(enemyHealth <= 0){
 			levelUp();
-		}	
+		}
 	}
 
-	console.log("you hit="+youHit);
-	console.log("damage=" + damage);
-	console.log("enemy Health=" + enemyHealth);
+	if(enemyHealth > 0){
+		defend();
+	}	
+}
+
+
+function block(){
+
+}
+
+function defend(){
+	var enemyHit = Math.floor(Math.random() * 20);
+
+	if(enemyHit){
+		console.log("enemy hit");
+		var damage = Math.floor(Math.random() * 5 + 1);
+		charHealth -= charHealth;
+		if(charHealth <= 0){
+			hide("attack");
+			show("dead");
+		}
+
+	}
+
 }
 
 function levelUp(xp){
 	show("level-up");
 	hide("attack");
+
+	charLvl = charLvl + 1;
 
 	console.log("you won");
 }
@@ -41,9 +68,9 @@ function levelUp(xp){
 
 
 function show (elementId) {
-	document.getElementById(elementId).style.display = "block";
+	document.getElementById(elementId).classList.remove("hidden");
 }
 
 function hide(elementId){
-	document.getElementById(elementId).style.display = "none";
+	document.getElementById(elementId).classList.add("hidden");
 }
